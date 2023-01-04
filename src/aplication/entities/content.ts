c3883f3b91b07228fea9a0f25c8470f632e8024d
Erika Perciliano <erikaperciliano.developer@gmail.com>
2023-01-04 09:02:15 +0000

    creates validation of the value of the content object

---
+++ src/aplication/entities/content.ts
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+export class Content {
+  private readonly content: string;
+
+  get Value(): string {
+    return this.content;
+    }
+
+  private validateContetLength(content: string): boolean {
+    return content.length >= 5 && content.length <= 240;
+    }
+
+  constructor(content: string) {
+    const isContentLengthValid = this.validateContetLength(content);
+
+    if (!isContentLengthValid) {
+      throw new Error('Content length error.');
+        }
+
+    this.content = content;
+    }
+}
